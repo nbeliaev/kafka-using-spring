f2122d0de1bdf01e516ef5f98f17b71f120eabba
Nick <fr13.dev@gmail.com>
2022-03-12 21:44:36 +0300

    add manual ack sending

--- library-events-consumer/src/main/java/com/learningkafka/config/ConsumerConfig.java
+++ library-events-consumer/src/main/java/com/learningkafka/config/ConsumerConfig.java
@@ -1,9 +1,26 @@
 package com.learningkafka.config;
 
+import org.springframework.beans.factory.ObjectProvider;
+import org.springframework.boot.autoconfigure.kafka.ConcurrentKafkaListenerContainerFactoryConfigurer;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.kafka.annotation.EnableKafka;
+import org.springframework.kafka.config.ConcurrentKafkaListenerContainerFactory;
+import org.springframework.kafka.core.ConsumerFactory;
 
 @Configuration
 @EnableKafka
 public class ConsumerConfig {
+
+    @Bean
+    ConcurrentKafkaListenerContainerFactory<?, ?> kafkaListenerContainerFactory(
+            ConcurrentKafkaListenerContainerFactoryConfigurer configurer,
+            ObjectProvider<ConsumerFactory<Object, Object>> kafkaConsumerFactory) {
+
+        ConcurrentKafkaListenerContainerFactory<Object, Object> factory = new ConcurrentKafkaListenerContainerFactory();
+        factory.setConcurrency(3);
+        //factory.getContainerProperties().setAckMode(ContainerProperties.AckMode.MANUAL);
+        configurer.configure(factory, kafkaConsumerFactory.getIfAvailable());
+        return factory;
+    }
 }
